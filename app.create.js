7a77d36f7fd8c121914fbfeeb377d1756e8dd934
toly <tolytrader@gmail.com>
2021-01-22 15:30:18 +0300

    init test data structure + init display test content

--- app.create.js
+++ app.create.js
@@ -1,6 +1,26 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    testData: {
+      title: 'presidents',
+      questions: [
+        {
+          text: 'who is president usa now?',
+          isSingeAnswer: true,
+          answers: [
+            {text: 'baiden', good: true},
+            {text: 'trump', good: false}
+          ]
+        },
+        {
+          text: 'who is president Russia now?',
+          isSingeAnswer: false,
+          answers: [
+            {text: 'Putin', good: true},
+            {text: 'Navalny', good: false}
+          ]
+        }
+      ]
+    }
   }
 })
