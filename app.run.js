0774a640b50666378c5817ce287a79f4d45c060b
toly <tolytrader@gmail.com>
2021-01-27 16:09:01 +0300

    working test stages

--- app.run.js
+++ app.run.js
@@ -1,14 +1,42 @@
+var questionType = {
+  text: 0,
+  radio: 1,
+  check: 2
+};
+
 let app = new Vue({
   el: '#app',
   data: {
+    questionType: questionType,
+    testLoaded: false,
+    testStarted: false,
+    testFinished: false,
+    currentQuestionIndex: 0,
+    currentQuestion: undefined,
 
+    answerText: undefined,
+    answerRadio: undefined,
+    answerCheck: [],
+
+    goodAnswersCount: 0
   },
   methods: {
-    loadTest: function() {
+    loadTest: function () {
       this.testData = JSON.parse(atob(testData));
+      console.log(this.testData);
+      this.currentQuestion = this.testData.questions[this.currentQuestionIndex];
+      this.testLoaded = true;
+    },
+    doAnswer: function () {
+      if (this.currentQuestionIndex + 1 < this.testData.questions.length) {
+        this.currentQuestionIndex += 1;
+        this.currentQuestion = this.testData.questions[this.currentQuestionIndex];
+      } else {
+        this.testFinished = true;
+      }
     }
   },
-  created: function() {
+  created: function () {
     this.loadTest();
   }
 });
